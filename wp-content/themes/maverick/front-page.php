13bce3ce20cc4fed6c1dbf27d7ba83575aaabc62
Matthew Kaiser <matt@kaiserdesigns.com>
2014-10-04 15:30:46 -0400

    wordpress updates and plugins

--- wp-content/themes/maverick/front-page.php
+++ wp-content/themes/maverick/front-page.php
@@ -7,6 +7,18 @@
 		<h2><?php the_title() ?></h2>
 		<?php the_content() ?>
 		
+		<h2>Recent Blog Posts</h2>
+		<?php
+		$args = array( 'numberposts' => '5' );
+		$recent_posts = wp_get_recent_posts( $args );
+		foreach( $recent_posts as $recent ){
+			echo '<li><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"].'</a> </li> ' . mysql2date('j M Y', $recent['post_date']);
+			echo "<pre>";
+			print_r($recent);
+			echo "</pre>"
+		}
+		?>
+		
 	<?php endwhile; ?>
 
 </main>

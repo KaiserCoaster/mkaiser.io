f9400f904e34ef4d50461e0cc41629c9498e3aa6
Matthew Kaiser <matt@kaiserdesigns.com>
2014-10-14 22:51:59 -0400

    Homepage updates
    Changed recent blog color, added featured projects and photography.
    grid switches to 1 column on mobile

--- wp-content/themes/maverick/front-page.php
+++ wp-content/themes/maverick/front-page.php
@@ -8,7 +8,7 @@
 		<div class="bottom-margin"><?php the_content() ?></div>
 		
 		<h2 class="bottom-margin">Recent Blog Posts</h2>
-		<ul id="recent_blogs" class="bottom-margin">
+		<ul id="recent_blogs" class="grid bottom-margin">
 			<?php
 			$args = array( 'numberposts' => '5' );
 			$recent_posts = wp_get_recent_posts( $args );
@@ -25,7 +25,6 @@
 				<?php
 			}
 			?>
-			
 				<li>
 					<div>
 						<date>8 June 2014</date>
@@ -41,7 +40,47 @@
 				<li>
 					<div>
 						<date>22 January 2014</date>
+						<a href="#">Daily GitHub Commits For a Year</a>
+					</div>
+				</li>
+		</ul>
+		
+		<h2 class="bottom-margin">Featured Projects</h2>
+		<ul id="featured_projects" class="grid bottom-margin">
+				<li>
+					<div>
+						<date>8 June 2014</date>
 						<a href="#">This is a title</a>
+					</div>
+				</li>
+				<li>
+					<div>
+						<date>22 January 2014</date>
+						<a href="#">This is a title</a>
+					</div>
+				</li>
+				<li>
+					<div>
+						<date>22 January 2014</date>
+						<a href="#">Daily GitHub Commits For a Year</a>
+					</div>
+				</li>
+		</ul>
+		
+		<h2 class="bottom-margin">Photography</h2>
+		<ul id="photography" class="grid bottom-margin">
+				<li>
+					<div>
+						<a href="#">
+							<img src="http://mkaiser.io/wp-content/uploads/2014/08/edited-5475-300x200.jpg" width="100%" />
+						</a>
+					</div>
+				</li>
+				<li>
+					<div>
+						<a href="#">
+							<img src="http://mkaiser.io/wp-content/uploads/2014/08/edited-5422-200x300.jpg" width="100%" />
+						</a>
 					</div>
 				</li>
 		</ul>
